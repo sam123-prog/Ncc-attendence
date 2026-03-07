7911a6a6aee5218612f16c73c99e715641d38807
Sai Samar <saisamar@outlook.com>
2026-03-07 11:51:18 +0530

    Add cadet and records data to Data.js

---
+++ Data.js
@@ -0,0 +1,17 @@
+const cadet = {
+name: "Rahul Sharma",
+reg: "NCC2025-041",
+attendance: "85%"
+}
+
+const records = [
+{date:"12 Jan", parade:"Drill", status:"Present"},
+{date:"19 Jan", parade:"Weapon Training", status:"Present"},
+{date:"26 Jan", parade:"Republic Day Parade", status:"Absent"}
+]
+
+const cadets = [
+{name:"Rahul Sharma", reg:"041", attendance:"85%"},
+{name:"Aman Singh", reg:"042", attendance:"72%"},
+{name:"Rohit Kumar", reg:"043", attendance:"90%"}
+]
